82105b0e2f977e73d3035c230b1f55e189907c82
abdo-mhmd <abdo_mhmd1@hotmail.com>
2023-06-20 06:45:53 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int i, n;
 	char chr[] = "_putchar";
+	
 	n = sizeof(chr);
 	for (i = 0; i < n; i++)
 	{
 		_putchar(chr[i]);
 	}
-	_putchar('\n');	
+	_putchar('\n');
 	return (0);
-}	
+}
