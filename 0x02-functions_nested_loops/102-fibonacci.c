b00cd92189c585a552d452d64e8f0cc6200292e0
abdo-mhmd <abdo_mhmd1@hotmail.com>
2023-06-20 15:06:41 -0700

    fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,14 @@ int main(void)
 	int max = 50;
 	int i, last = 1, now = 2, next;
 
+	printf("%d, %d", first, second);
 	for (i = 3; i <= max; i++)
 	{
 		next = last + now;
 		last = now;
 		now = next;
 		if (i == max)
-			printf("%d",now);
+			printf("%d", now);
 		else
 			printf("%d ,", now);
 	}
