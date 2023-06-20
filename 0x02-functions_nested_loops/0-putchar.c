dc56e4315793bd3394dc0fbc8c0dd11868bf122f
abdo-mhmd <abdo_mhmd1@hotmail.com>
2023-06-20 06:35:13 -0700

    putchar add

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description:
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n;
+	char chr[] = "_putchar";
+	n = sizeof(chr);
+	for (i = 0; i < n; i++)
+	{
+		_putchar(chr[i]);
+	}
+	_putchar('\n');	
+	return (0);
+}	
