0a84512d5cd9845f6d0a0b9bcd113e53473ff554
abdo-mhmd <abdo_mhmd1@hotmail.com>
2023-06-20 13:27:13 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,39 @@
+#include <stdio>
+/**
+ * main -main block
+ * Description:
+ * Return:0
+ */
+int main(void)
+{
+	int max = 1024;
+	int sum = 0;
+	int i, equal;
+
+	for (i = 1; i < max; i++)
+	{
+		equal = i * 3;
+		if (equal < max)
+		{
+			sum += equal;
+		}
+		else
+		{
+			break;
+		}
+	}
+	for (i = 1; i < max; i++)
+	{
+		equal = i * 5;
+		if (equal < max)
+		{
+			sum += equal;
+		}
+		else
+		{
+			break;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
