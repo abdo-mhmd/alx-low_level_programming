b975259d1f03f61052bbebf9bb2dd605d0d4757c
abdo-mhmd <abdo_mhmd1@hotmail.com>
2023-06-20 12:53:09 -0700

    times table2

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - print time table
+ * @n :git intager
+ * Return: void
+ */
+int print_times_table(int n)
+{
+	int i, x, equal;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (x = 0; x <= n; x++)
+			{
+				equal = x * i;
+				if (x != 0 && equal < 10)
+					printf("   %d", equal);
+				else if (x != 0 && equal < 100)
+					printf("  %d", equal);
+				else if (x != 0 && equal >= 100)
+					printf(" %d", equal);
+				else
+					printf("%d", equal);
+
+				if (x != n)
+					printf(",");
+			}
+			printf("\n");
+		}
+	}
+}
